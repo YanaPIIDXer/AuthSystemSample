6163daf8fccf986d3711aec7f1bd8e76acb83ec1
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-26 21:05:55 +0900

    新規登録とログインを分けた

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,9 +7,9 @@ const password = ref("");
 const isProcessing = ref(false);
 
 /**
- * 送信
+ * ログイン
  */
-const submit = async () => {
+const submitLogin = async () => {
   isProcessing.value = true;
   try {
     const body = {
@@ -24,24 +24,45 @@ const submit = async () => {
   } finally {
     isProcessing.value = false;
   }
-}
+};
+
+/**
+ * 新規登録
+ */
+const submitRegister = async () => {
+  isProcessing.value = true;
+  try {
+    const body = {
+      userId: userId.value,
+      password: password.value,
+    }
+    const response = await axios.post("/register", body);
+    console.log(response);
+  } catch (error: any) {
+    alert(error.message ? error.message : "何かエラー");
+    console.error(error);
+  } finally {
+    isProcessing.value = false;
+  }
+};
 
 defineExpose({
   userId,
   password,
   isProcessing,
-  submit
+  submitLogin,
 });
 </script>
 
 <template lang="pug">
 .root
-  form(@submit.prevent="submit")
+  form(@submit.prevent="submitLogin")
     input(type="text" v-model="userId" placeholder="ユーザID")
     br
     input(type="password" v-model="password" placeholder="パスワード")
     br
     input(type="submit" value="ログイン" :disabled="(!userId || !password) || isProcessing")
+    input(type="button" @click="submitRegister" value="新規登録" :disabled="(!userId || !password) || isProcessing")
 </template>
 
 <style lang="sass" scoped>
