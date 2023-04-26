c6a5055d77283aebf2e4f4239de7f8b76c8b3ce8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-26 21:02:42 +0900

    とりあえずログイン処理

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,47 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import axios from "@/modules/Axios";
+
+const userId = ref("");
+const password = ref("");
+const isProcessing = ref(false);
+
+/**
+ * 送信
+ */
+const submit = async () => {
+  isProcessing.value = true;
+  try {
+    const body = {
+      userId: userId.value,
+      password: password.value,
+    }
+    const response = await axios.post("/login", body);
+    console.log(response);
+  } catch (error: any) {
+    alert(error.message ? error.message : "何かエラー");
+    console.error(error);
+  } finally {
+    isProcessing.value = false;
+  }
+}
+
+defineExpose({
+  userId,
+  password,
+  isProcessing,
+  submit
+});
 </script>
 
 <template lang="pug">
+.root
+  form(@submit.prevent="submit")
+    input(type="text" v-model="userId" placeholder="ユーザID")
+    br
+    input(type="password" v-model="password" placeholder="パスワード")
+    br
+    input(type="submit" value="ログイン" :disabled="(!userId || !password) || isProcessing")
 </template>
 
 <style lang="sass" scoped>
