c379e110c9e217d598c0a6959bb7e7572db4ba09
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-26 21:12:06 +0900

    とりあえずAPIのエンドポイントだけ定義

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,8 @@
+import register from "./api/register";
+import login from "./api/login";
+import { Application } from "express";
+
+export const addRoutes = (app: Application): void => {
+  app.post("/login", login);
+  app.post("/register", register);
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import Express from "express";
 import CORS from "cors";
+import { addRoutes } from "./routes";
 
 const app = Express();
 app.use(Express.json());
 app.use(CORS());
+
+addRoutes(app);
 
 app.listen(3000, () => {
   console.info("Server is Running...");
